56570e10d9604cb6b9424ed1922151d3fa30bc80
aligator <aligator@suncraft-server.de>
2019-07-05 23:09:23 +0200

    lorca with asset generator

--- main.go
+++ main.go
@@ -1,24 +1,56 @@
+//go:generate go run -tags generate gen.go
+
 package main
 
 import (
+	"fmt"
 	"github.com/zserge/lorca"
 	"log"
-	"net/url"
+	. "net"
+	"net/http"
+	"os"
+	"os/signal"
+	"runtime"
 )
 
 func main() {
-	// Create UI with basic HTML passed via data URI
-	ui, err := lorca.New("data:text/html,"+url.PathEscape(`
-	<html>
-		<head><title>coffee2go</title></head>
-		<body><h1>Drink coffee2go and chat with XMPP</h1></body>
-		
-	</html>
-	`), "", 480, 320)
+	args := []string{}
+	if runtime.GOOS == "linux" {
+		args = append(args, "--class=Lorca")
+	}
+	ui, err := lorca.New("", "", 480, 320, args...)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer ui.Close()
-	// Wait until UI window is closed
-	<-ui.Done()
+
+	// A simple way to know when UI is ready (uses body.onload event in JS)
+	ui.Bind("start", func() {
+		log.Println("UI is ready")
+	})
+
+	ln, err := Listen("tcp", "127.0.0.1:0")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ln.Close()
+	go http.Serve(ln, http.FileServer(FS))
+	ui.Load(fmt.Sprintf("http://%s", ln.Addr()))
+
+	// You may use console.log to debug your JS code, it will be printed via
+	// log.Println(). Also exceptions are printed in a similar manner.
+	ui.Eval(`
+		console.log("Hello, world!");
+		console.log('Multiple values:', [1, false, {"x":5}]);
+	`)
+
+	// Wait until the interrupt signal arrives or browser window is closed
+	sigc := make(chan os.Signal)
+	signal.Notify(sigc, os.Interrupt)
+	select {
+	case <-sigc:
+	case <-ui.Done():
+	}
+
+	log.Println("exiting...")
 }
