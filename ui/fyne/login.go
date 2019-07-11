facc428082c37fda35efe82ebbeedb5cf2cf64bc
Johannes Hörmann <aligator@users.noreply.github.com>
2019-07-11 13:05:47 +0200

    better login layout

--- ui/fyne/login.go
+++ ui/fyne/login.go
@@ -1,12 +1,11 @@
 package fyne
 
 import (
-	"code.gitea.io/gitea/modules/log"
+	"log"
 	"fmt"
 	"fyne.io/fyne"
 	"fyne.io/fyne/layout"
 	"fyne.io/fyne/widget"
-	"github.com/jdc-lab/coffee2go/ui/fyne/customlayout"
 )
 
 type Login struct {
@@ -26,7 +25,7 @@ func NewLogin(m *Master) *Login {
 		server:   widget.NewEntry(),
 		username: widget.NewEntry(),
 		password: widget.NewPasswordEntry(),
-		login:    widget.NewButton("go", nil),
+		login:    widget.NewButton("Drink Coffee2Go", nil),
 	}
 
 	return &l
@@ -65,14 +64,97 @@ func (l *Login) PrefillForm(server, username, password string) {
 }
 
 func (l *Login) LoadLogin() {
-	l.window.Resize(fyne.NewSize(150, 150))
+	l.window.Resize(fyne.NewSize(300, 0))
+	l.window.SetFixedSize(true)
+	
 	l.window.SetContent(fyne.NewContainerWithLayout(layout.NewGridLayout(1),
-		fyne.NewContainerWithLayout(customlayout.NewHCenterLayout(), l.name),
-		fyne.NewContainerWithLayout(layout.NewMaxLayout(),
-			fyne.NewContainerWithLayout(layout.NewVBoxLayout(),
-				l.server, l.username, l.password, l.login))))
+		fyne.NewContainerWithLayout(layout.NewFormLayout(),	
+			widget.NewLabel("Server:"),
+			l.server,
+			widget.NewLabel("Username:"),
+			l.username,
+			widget.NewLabel("Passwort:"),
+			l.password),
+		fyne.NewContainerWithLayout(layout.NewCenterLayout(), l.login)))
+	l.onLoginLoaded()
+}
+package fyne
 
-	l.name.Move(fyne.NewPos(0, 0))
+import (
+	"log"
+	"fmt"
+	"fyne.io/fyne"
+	"fyne.io/fyne/layout"
+	"fyne.io/fyne/widget"
+)
 
+type Login struct {
+	*Master
+	onLoginLoaded func()
+	name          *widget.Label
+	server        *widget.Entry
+	username      *widget.Entry
+	password      *widget.Entry
+	login         *widget.Button
+}
+
+func NewLogin(m *Master) *Login {
+	l := Login{
+		Master:   m,
+		name:     widget.NewLabel("Coffee2Go"),
+		server:   widget.NewEntry(),
+		username: widget.NewEntry(),
+		password: widget.NewPasswordEntry(),
+		login:    widget.NewButton("Drink Coffee2Go", nil),
+	}
+
+	return &l
+}
+
+func (l *Login) Close() {
+	fmt.Println("Close not implemented")
+}
+
+func (l *Login) Bind(name string, f interface{}) error {
+	switch name {
+	case "goOnLoginLoaded":
+		if f, ok := f.(func()); ok {
+			l.onLoginLoaded = f
+		} else {
+			log.Fatal("Binding is not of correct function-type", name)
+		}
+	case "goLogin":
+		if f, ok := f.(func(server, username, password string)); ok {
+			l.login.OnTapped = func() {
+				f(l.server.Text, l.username.Text, l.password.Text)
+			}
+		} else {
+			log.Fatal("Binding is not of correct function-type", name)
+		}
+	default:
+		log.Fatal("Binding not implemented in fyne", name)
+	}
+	return nil
+}
+
+func (l *Login) PrefillForm(server, username, password string) {
+	l.server.SetText(server)
+	l.username.SetText(username)
+	l.password.SetText(password)
+}
+
+func (l *Login) LoadLogin() {
+	l.window.Resize(fyne.NewSize(300, 0))
+	l.window.SetFixedSize(true)
+	
+	l.window.SetContent(fyne.NewContainerWithLayout(layout.NewGridLayout(1),
+		fyne.NewContainerWithLayout(layout.NewFormLayout(),	
+			widget.NewLabel("Server:"),
+			l.server,
+			widget.NewLabel("Username:"),
+			l.username,
+			widget.NewLabel("Passwort:"),
+			l.password),
+		fyne.NewContainerWithLayout(layout.NewCenterLayout(), l.login)))
 	l.onLoginLoaded()
 }
