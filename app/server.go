e6378d27984fbd5aeeef8b009e53b82bf545f15b
aligator <aligator@suncraft-server.de>
2019-07-27 10:43:16 +0200

    fix fileserver serving wrong mime type

--- app/server.go
+++ app/server.go
@@ -6,8 +6,8 @@ import (
 	"github.com/google/uuid"
 	"html/template"
 	"net/http"
+	"strings"
 )
-
 
 type Login struct {
 	Hostname string `json:"hostname"`
@@ -51,8 +51,27 @@ func (s *Server) setupWeb(router *chi.Mux) {
 		tmpl.Execute(w, login)
 	})
 
-	fs := http.FileServer(http.Dir("client/static"))
-	router.Handle("/static*", fs)
+	FileServer(router, "/static", http.Dir("client/static"))
+}
+
+// FileServer conveniently sets up a http.FileServer handler to serve
+// static files from a http.FileSystem.
+func FileServer(r chi.Router, path string, root http.FileSystem) {
+	if strings.ContainsAny(path, "{}*") {
+		panic("FileServer does not permit URL parameters.")
+	}
+
+	fs := http.StripPrefix(path, http.FileServer(root))
+
+	if path != "/" && path[len(path)-1] != '/' {
+		r.Get(path, http.RedirectHandler(path+"/", 301).ServeHTTP)
+		path += "/"
+	}
+	path += "*"
+
+	r.Get(path, func(w http.ResponseWriter, r *http.Request) {
+		fs.ServeHTTP(w, r)
+	})
 }
 
 func parseFlags() (hostname, username, password *string) {
